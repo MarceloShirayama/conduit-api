ee719df184c57ea2e620c884d1303eee9cf74a88
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-03-02 17:41:58 -0300

    refactor: mapAll types

--- src/test/config/fixtures/index.ts
+++ src/test/config/fixtures/index.ts
@@ -5,11 +5,11 @@ export function unsafe<T>(value: unknown): T {
   return value as T;
 }
 
-type Callback = (a: unknown) => unknown;
+type Callback<E, T> = (a: E | T) => unknown;
 
-type MapAll = (
-  fn: Callback
-) => (data: TE.TaskEither<unknown, unknown>) => TE.TaskEither<unknown, unknown>;
+type MapAll = <E, T>(
+  fn: Callback<E, T>
+) => (data: TE.TaskEither<E, T>) => TE.TaskEither<unknown, unknown>;
 
 export const mapAll: MapAll = (fn) => (data) => {
   return pipe(data, TE.map(fn), TE.mapLeft(fn));
